ad392d256ec9a035c6e83d94c4d41b6d9119398b
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2022-09-19 07:25:44 -0400

    Update metricas.py

--- pages/metricas.py
+++ pages/metricas.py
@@ -7,26 +7,25 @@ import seaborn as sns
 import matplotlib as plt
 import numpy as np
 from funciones import *
-try:
-    df_prods= st.session_state['saved_nets']
-    df_prods['index']= np.arange(1, df_prods.shape[0] + 1)
-    df_prods.index = df_prods['index']
-    st.write(df_prods.index)
-    st.header('Analisis de la red seleccionada')
-    st.dataframe(df_prods)
-    if st.button('Limpiar'):
-        st.session_state['saved_nets']= pd.DataFrame()
-    csv = convert_df(df_prods)
 
-    st.download_button(
-    "Descargar",
-    csv,
-    "file.csv",
-    "text/csv",
-    key='download-csv'
-    )
-except:
-    st.warning('Seleccione al menos una red de productos o contractual')
+df_prods= st.session_state['saved_nets']
+df_prods['index']= np.arange(1, df_prods.shape[0] + 1)
+df_prods.index = df_prods['index']
+st.write(df_prods.index)
+st.header('Analisis de la red seleccionada')
+st.dataframe(df_prods)
+if st.button('Limpiar'):
+    st.session_state['saved_nets']= pd.DataFrame()
+csv = convert_df(df_prods)
+
+st.download_button(
+"Descargar",
+csv,
+"file.csv",
+"text/csv",
+key='download-csv'
+)
+
     
 
 
