b06492441d08ef910527ccfb6fc080da6911930a
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2022-09-20 06:34:47 -0400

    Update metricas.py

--- pages/metricas.py
+++ pages/metricas.py
@@ -27,7 +27,7 @@ csv,
 key='download-csv'
 )
 fig = plt.figure.Figure(figsize=(10, 4))
-sns.countplot(x="num_nodos", data=df_prods)
+sns.barplot(data=df_prods, x="index", y= 'num_nodes', hue= 'densidad' )
 st.pyplot(fig)
 
 
