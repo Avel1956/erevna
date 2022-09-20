a3bb12a7734a9628565d67f00ed03a2f94170dca
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2022-09-20 08:51:38 -0400

    Update metricas.py

--- pages/metricas.py
+++ pages/metricas.py
@@ -27,7 +27,7 @@ csv,
 key='download-csv'
 )
 fig = plt.figure.Figure(figsize=(10, 4))
-sns.catplot(data=df_prods, x="index", y= 'num_nodos', hue= 'Cuantas subredes' )
+sns.catplot(data=df_prods, x="index", y= 'num_nodos' )
 st.pyplot(fig)
 
 
