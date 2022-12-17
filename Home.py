aa7602ccb84f4994c97d37a74330849a1174914b
Avel1956 <35372097+Avel1956@users.noreply.github.com>
2022-12-17 09:05:51 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -141,10 +141,7 @@ try:
     # #Dataframe de métricas de la red para representación  
     red_metrics_df = red_metrics(color_net, query)
 
-    #histograma de grado de los nodos de la red
-    hist_grado = hist_plot(color_net, 'grado')
-    #save figure
-    #hist_grado.savefig('output\\hist_grado.png')
+    
 
   
     #######
@@ -218,8 +215,7 @@ try:
         st.subheader('Métricas del nodo seleccionado')
         st.dataframe(node_metrics_df)
         st.subheader('Métricas guardadas')
-        #show histogram
-        #st.image('output\\hist_grado.png')
+        
         st.dataframe(df_node_metrics)
 
 except:
